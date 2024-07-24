1131bafee1e676bcce73793fc92ab0ee055c0c86
mar1do12 <marin.benis@orange.fr>
2024-07-24 16:26:49 +0200

    Update main.py

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,8 +1,4 @@
 def main():
-    # Ecrivez votre code ici !
-    # Attention tout votre code doit être indenté comme ce commentaire
-
-
-# Ne touchez pas le code ci-dessous
+   liste = input("Saisissez une liste de nombre séparés par une virgule
 if __name__ == "__main__":
     main()
